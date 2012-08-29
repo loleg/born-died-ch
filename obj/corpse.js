53a0c2f2245b424aa830f97cd4d3ae01218fbb1e
Oleg Lavrovsky <oleg@utou.ch>
2012-08-29 15:13:00 +0200

    Initial commit

---
+++ obj/corpse.js
@@ -0,0 +1,34 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "generatedBy"   : "Blender 2.63 Exporter",
+        "vertices"      : 0,
+        "faces"         : 0,
+        "normals"       : 0,
+        "colors"        : 0,
+        "uvs"           : 0,
+        "materials"     : 0,
+        "morphTargets"  : 0
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [],
+
+    "vertices": [],
+
+    "morphTargets": [],
+
+    "normals": [],
+
+    "colors": [],
+
+    "uvs": [[]],
+
+    "faces": []
+
+
+
+}
